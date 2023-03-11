04944c230aaca0beb5342dbac6668ce2b3d0dfa3
Coshido <coshido@gmail.com>
2023-03-11 10:08:30 +0100

    add readme

--- src/scenes/dashboard/index.jsx
+++ src/scenes/dashboard/index.jsx
@@ -18,7 +18,7 @@ const Dashboard = () => {
   const colors = tokens(theme.palette.mode);
 
   return (
-    <Box m="20px">
+    <Box m="10px 20px">
       <Box display="flex" justifyContent="space-between" alignItems="center">
         <Header title="DASHBOARD" subtitle="Welcome to your dashboard" />
 
